bcc5bacdec15e77efd9d7eecf413bc448dc62868
Matthew del Real <matthewadelreal@gmail.com>
2023-10-22 23:47:05 -0400

    Adding the javadoc stuff to branch

--- main.java
+++ main.java
@@ -1,15 +1,31 @@
-class Main {
+/**
+ * @author Matthew del Real *
+ */
+public class main {
+  //this is the main class
+
+  /**
+   *
+   * @param args this is the default main class
+   */
   public static void main(String[] args) {
+    //main method
 
-    int num = 10;
+    int num = 10; //the number that will be passed
 
-    int fib = recFib(num);
-    System.out.println("The "+ num +"th term of the Fibonacci sequence is " + fib);
+    int fib = recFib(num); //the resulting number with the passed 10
+    System.out.println("The "+ num +"th term of the Fibonacci sequence is " + fib); //print result
   }
 
 
   //recursive fib sequence
   //then printing results after
+
+  /**
+   *
+   * @param num this is the number of the fibonacci sequence that is to be created
+   * @return returns the nth number of the fibonacci sequence
+   */
   public static int recFib(int num){
     if(num == 0)
       return 0;
---
+++ out/production/cen4802/README.md
@@ -0,0 +1,2 @@
+# cen4802
+Matthew del Real repo of Assignment 2.
